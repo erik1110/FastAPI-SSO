537e57169259fe99bbbe08af35f31b30f1629096
erik1110 <oceanuheart@gmail.com>
2023-08-04 16:19:37 +0800

    [feat] remove app

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 from pydantic import BaseSettings
 
-from app.models.customer import Customer
+from models.customer import Customer
 
 class Settings(BaseSettings):
     # database configurations
